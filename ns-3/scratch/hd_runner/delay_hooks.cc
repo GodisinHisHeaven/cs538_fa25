76f95609f88ff37dbd8433759ccd806c6e038f00
Edwin Ing <edwinji2@illinois.edu>
2025-10-19 09:06:43 -0500

    Integrated a simple delay hook.
    As discussed in the meeting on Friday, I implemented the delay hook for my simple model based on the Understanding Host Network Paper. It utilizes measurements of both C2M and P2M measured in the paper while varing the number of C2M cores. The default for the experiment has been set to 6 since that will induce the largest delay.

--- ns-3/scratch/hd_runner/delay_hooks.cc
+++ ns-3/scratch/hd_runner/delay_hooks.cc
@@ -8,10 +8,39 @@
 
 #include "delay_hooks.h"
 #include "ns3/log.h"
-
+#include <iostream>
 namespace ns3 {
 
 NS_LOG_COMPONENT_DEFINE("DelayHooks");
+
+// Latency data in nanoseconds
+static const uint32_t C2M[] = {30, 70, 105, 135, 160, 205};
+static const uint32_t P2M[] = {30, 70, 110, 200, 240, 270};
+static const size_t LATENCY_ARRAY_SIZE = 6;
+
+/**
+ * Calculate total latency = 2 * P2M[i-1] + 2 * C2M[i-1]
+ * where i is the number of C2M cores (1 to 6)
+ */
+uint32_t CalculateTotalLatency(uint32_t numCores)
+{
+    if (numCores < 1 || numCores > LATENCY_ARRAY_SIZE)
+    {
+        NS_LOG_ERROR("Invalid number of cores: " << numCores
+                     << " (must be between 1 and 6)");
+        return 0;
+    }
+
+    uint32_t index = numCores - 1;
+    uint32_t totalLatency = 2 * P2M[index] + 2 * C2M[index];
+
+    std::cout << "Latency calculation for " << numCores << " C2M cores: "
+                 << "2*P2M[" << index << "] + 2*C2M[" << index << "] = "
+                 << "2*" << P2M[index] << " + 2*" << C2M[index] << " = "
+                 << totalLatency << " ns" << std::endl;
+
+    return totalLatency;
+}
 
 // Static member initialization
 bool DelayHooks::s_egressEnabled = false;
@@ -19,8 +48,7 @@ bool DelayHooks::s_ingressEnabled = false;
 std::string DelayHooks::s_configPath = "";
 uint32_t DelayHooks::s_seed = 0;
 
-void
-DelayHooks::Initialize(const std::string& configPath,
+void DelayHooks::Initialize(const std::string& configPath,
                        bool enableEgress,
                        bool enableIngress,
                        uint32_t seed)
@@ -35,11 +63,10 @@ DelayHooks::Initialize(const std::string& configPath,
     NS_LOG_INFO("  Ingress enabled: " << (enableIngress ? "yes" : "no"));
     NS_LOG_INFO("  Config path: " << (configPath.empty() ? "(none)" : configPath));
     NS_LOG_INFO("  Seed: " << seed);
-    NS_LOG_INFO("  Current behavior: NO-OP (zero delay)");
+    NS_LOG_INFO("  Current behavior: Ingress uses C2M core 6 latency");
 }
 
-Time
-DelayHooks::DelayEgress(uint32_t nodeId, uint32_t bytes, uint32_t seq)
+Time DelayHooks::DelayEgress(uint32_t nodeId, uint32_t bytes, uint32_t seq)
 {
     if (!s_egressEnabled)
     {
@@ -55,8 +82,7 @@ DelayHooks::DelayEgress(uint32_t nodeId, uint32_t bytes, uint32_t seq)
     return Time(0);
 }
 
-Time
-DelayHooks::DelayIngress(uint32_t nodeId, uint32_t bytes, uint32_t seq)
+Time DelayHooks::DelayIngress(uint32_t nodeId, uint32_t bytes, uint32_t seq)
 {
     if (!s_ingressEnabled)
     {
@@ -67,19 +93,20 @@ DelayHooks::DelayIngress(uint32_t nodeId, uint32_t bytes, uint32_t seq)
                  << " bytes=" << bytes
                  << " seq=" << seq);
 
-    // NO-OP: Return zero delay
-    // Future implementation will calculate delay based on s_configPath
-    return Time(0);
+    // Calculate latency for C2M core 6
+    uint32_t latencyNs = CalculateTotalLatency(6);
+
+    std::cout << "Adding ingress latency: " << latencyNs << " ns" << std::endl;
+
+    return NanoSeconds(latencyNs);
 }
 
-bool
-DelayHooks::IsEgressEnabled()
+bool DelayHooks::IsEgressEnabled()
 {
     return s_egressEnabled;
 }
 
-bool
-DelayHooks::IsIngressEnabled()
+bool DelayHooks::IsIngressEnabled()
 {
     return s_ingressEnabled;
 }
